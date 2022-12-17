4cf316767ee72eeae81da77444703ae1105d6a51
Ike Mulder <ikemulder@hotmail.com>
2022-12-17 10:24:18 +0100

    Day 14 initial

---
+++ day14.py
@@ -0,0 +1,11 @@
+# populate a dictionary with rock monoliths. no need to store whether sand or rock
+# get lowest rock monolith
+# simulate sand falling until stopped or lower than lowest rock monolith
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
